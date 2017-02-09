95771cf1ab872fc5e3cab61ad6b201670a635aa5
Benjamin Rozière <benjamin.roziere@rdalambic.com>
2017-02-09 16:45:07 +0100

    Add main

--- main.c
+++ main.c
@@ -3,13 +3,37 @@
 //
 
 #include <printf.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include "helper.h"
+#include "parser.h"
 
 int main(int argc, char ** argv)
 {
-    char * path;
+    char * path, buf;
+    int choix;
+    parser * p = new(parser);
+    list_t agenda = init_list();
 
     if(argc > 1)
     {
+        malcx(path, sizeof(char)*strlen(argv[1]), "Allocation argument impossible")
+        strcpy(path, argv[1]);
+        if(p->Charger(path, &agenda) != RETURN_SUCCESS)
+        {
+            fprintf(stderr, "Impossible de lire le fichier");
+            return -1;
+        }
+
+        do
+        {
+            fprintf(stdout, "Action : ");
+            fscanf(stdin, "%d%*c", &choix);
+
+        }while (choix != 0);
+
+        p->Sauvegarder(path, agenda);
 
     }
     else
