d4d840eb0a4b8a00be6f199e0dbfce9443e0723f
Benjamin Rozière <benjamin.roziere@rdalambic.com>
2017-02-09 16:32:38 +0100

    Fix new macro

--- main.c
+++ main.c
@@ -2,7 +2,20 @@
 // Created by Benjamin Rozière on 03/02/2017.
 //
 
+#include <printf.h>
+
 int main(int argc, char ** argv)
 {
+    char * path;
 
+    if(argc > 1)
+    {
+
+    }
+    else
+    {
+        fprintf(stderr, "Vous devez spécifier un fichier en argument\n");
+    }
+
+    return 0;
 }
