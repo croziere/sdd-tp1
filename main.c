bc1b20a8dee27eebf59767bbb084bf28824a3a64
Benjamin Rozière <benjamin.roziere@rdalambic.com>
2017-03-10 14:18:43 +0100

    Cosmetic corrections

--- main.c
+++ main.c
@@ -1,13 +1,7 @@
-//
-// Created by Benjamin Rozière on 03/02/2017.
-//
 #include "controller.h"
 
 int main(int argc, char ** argv)
 {
     /* Execute le programme principal */
     return controller_main(argc, argv);
-
-    /* Enlever les commentaires pour tester */
-    /* TODO */
 }
